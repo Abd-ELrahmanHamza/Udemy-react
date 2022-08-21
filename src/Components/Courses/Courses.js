9571a666829cedc44220e6e396deca1c018406cb
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-08-21 17:40:18 +0200

    feat: create components

---
+++ src/Components/Courses/Courses.js
@@ -0,0 +1,8 @@
+// Import CSS
+import "./Courses.css";
+
+const Courses = () => {
+  return <></>;
+};
+
+export default Courses;
---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,12 @@
+// Import components
+import Courses from "../../Components/Courses/Courses";
+
+const Home = () => {
+  return (
+    <>
+      <Courses></Courses>
+    </>
+  );
+};
+
+export default Home;
