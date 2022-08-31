a4f3db44f845e2fd5340077f2f8bc8e475fd647d
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-08-31 23:56:05 +0200

    feat: install react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// Import css
 import "./App.css";
+
+// Import components
 import Home from "./Containers/Home/Home";
+
+// Import react router dom
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home></Home>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
