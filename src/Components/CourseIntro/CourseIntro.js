998e88ba18d37842792e840f3185dd410c708009
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-09-01 03:07:18 +0200

    feat: create course introduction

---
+++ src/Components/CourseIntro/CourseIntro.js
@@ -0,0 +1,63 @@
+// Import css
+import styles from "./CourseIntro.module.css";
+
+// Import react icons
+import {
+  BsStarHalf,
+  BsStarFill,
+  BsExclamationOctagonFill,
+  BsGlobe,
+} from "react-icons/bs";
+import { FaClosedCaptioning } from "react-icons/fa";
+
+// Import react router dom
+import { Link } from "react-router-dom";
+
+const CourseIntro = () => {
+  return (
+    <>
+      <h1 className={styles["title"]}>
+        Python Beyond the Basics - Object-Oriented Programming
+      </h1>
+      CourseHeader
+      <h2 className={styles["description"]}>
+        From Classes To Inheritance - OOP In-Depth For Python Programmers
+      </h2>
+      <div className={styles["stars"]}>
+        <span className={styles["course-rate"]}>
+          {Math.round(4.3 * 10) / 10}
+        </span>
+        <BsStarFill />
+        <BsStarFill />
+        <BsStarFill />
+        <BsStarFill />
+        <BsStarHalf />
+        <i className="fa-solid fa-star-half-stroke"></i>
+        <Link to="/" className={styles["course-ratings"]}>
+          ({2.944} ratings)
+        </Link>
+        <span className={styles["course-students"]}>10,488 students</span>
+      </div>
+      <p className={styles["created-by"]}>
+        Created by
+        <span className={styles["course-ratings"]}>infinite Skills</span>
+      </p>
+      <div className={styles["captions"]}>
+        <div className={styles["captions-inner"]}>
+          <BsExclamationOctagonFill />
+          <span> Last updated on 9/2022 </span>
+        </div>
+        <div className={styles["captions-inner"]}>
+          <BsGlobe />
+          <span> English </span>
+        </div>
+        <div className={styles["captions-inner"]}>
+          <FaClosedCaptioning />
+          <span> English </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CourseIntro;
--- src/Containers/Course/Course.js
+++ src/Containers/Course/Course.js
@@ -1,12 +1,19 @@
 // Import css
-import "./Course.css";
+import styles from "./Course.module.css";
 
 // Import components
 import Footer from "../../Components/Footer/Footer";
+import CourseIntro from "../../Components/CourseIntro/CourseIntro";
 
 const Course = () => {
   return (
     <>
+      <div className={styles["black-back"]}></div>
+      <div className={styles["container"]}>
+        <div className={styles["content-container"]}>
+          <CourseIntro />
+        </div>
+      </div>
       <Footer />
     </>
   );
