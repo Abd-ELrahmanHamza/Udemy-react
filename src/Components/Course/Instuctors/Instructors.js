44f1632e8933fcca1cf5bd03d9e4ac5316f8efe2
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-09-03 04:32:14 +0200

    feat: instructor image

--- src/Components/Course/Instuctors/Instructors.js
+++ src/Components/Course/Instuctors/Instructors.js
@@ -4,15 +4,22 @@ import styles from "./Instructors.module.css";
 // Import react router dom components
 import { Link } from "react-router-dom";
 
+// Import image
+import instructor from "../../../Assets/Images/instructor.jpg";
 const Instructors = () => {
   return (
     <div>
       <h2>Instructors</h2>
-      <h2>
-        <Link to={"/"} className={styles["Instructor-link"]}>
-          Instructor link
+      <h2 className={styles["instructor-link"]}>
+        <Link to={"/"} className={styles["instructor-link"]}>
+          Stephane Maarek | AWS Certified Cloud Practitioner,Solutions
+          Architect,Developer
         </Link>
       </h2>
+      <p className={styles["instructor-quote"]}>
+        Best Selling Instructor, 9x AWS Certified, Kafka Guru
+      </p>
+      <img src={instructor} alt="instructor" className={styles["instructor-image"]} />
     </div>
   );
 };
