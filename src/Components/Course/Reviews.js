be19aa7cb2d77b1b3849aa4c22ec3ddff437b154
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-09-07 00:37:37 +0200

    feat: add rating select

--- src/Components/Course/Reviews.js
+++ src/Components/Course/Reviews.js
@@ -3,20 +3,37 @@ import styles from "../../Style/Components/Course/Reviews.module.css";
 
 // Import react icons
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Reviews = () => {
   return (
     <div className={styles["reviews-container"]}>
       <h2>Reviews</h2>
       <div className={styles["search-container"]}>
-        <input
-          type={"text"}
-          placeholder="Search reviews"
-          className={styles["search-input"]}
-        />
-        <button className={styles["search-icon-back"]}>
-          <AiOutlineSearch size={30} className={styles["search-icon"]} />
-        </button>
+        <div className={styles["search"]}>
+          <input
+            type={"text"}
+            placeholder="Search reviews"
+            className={styles["search-input"]}
+          />
+          <button className={styles["search-icon-back"]}>
+            <AiOutlineSearch size={30} className={styles["search-icon"]} />
+          </button>
+        </div>
+
+        <div className={styles["select-star-container"]}>
+          <select className={styles["select-star"]} name="star" id="star">
+            <option value="All ratings">All ratings</option>
+            <option value="Five stars">Five stars</option>
+            <option value="Four stars">Four stars</option>
+            <option value="Three stars">Three stars</option>
+            <option value="Two stars">Two stars</option>
+            <option value="One star">One stars</option>
+          </select>
+          <span className={styles["arrow-icon"]}>
+            <IoIosArrowDown />
+          </span>
+        </div>
       </div>
     </div>
   );
