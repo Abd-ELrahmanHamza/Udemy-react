a1d26af10d607ae3e9d6a15975893574308d002a
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-09-08 00:53:39 +0200

    feat: style header for small screens

--- src/Components/Course/Header.js
+++ src/Components/Course/Header.js
@@ -9,15 +9,23 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className={styles["header"]}>
-      <h2 className={styles["title"]}>Learn Python: The Complete Python Programming Course</h2>
       <div>
-        <span className={styles["course-rate"]}>
-          {Math.round(4.3 * 10) / 10} <BsStarFill />
-        </span>
-        <Link to="/" className={styles["course-ratings"]}>
-          ({2.944} ratings)
-        </Link>
-        <span className={styles["course-students"]}>10,488 students</span>
+        <h2 className={styles["title"]}>
+          Learn Python: The Complete Python Programming Course
+        </h2>
+        <div>
+          <span className={styles["course-rate"]}>
+            {Math.round(4.3 * 10) / 10} <BsStarFill />
+          </span>
+          <Link to="/" className={styles["course-ratings"]}>
+            ({2.944} ratings)
+          </Link>
+          <span className={styles["course-students"]}>10,488 students</span>
+        </div>
+      </div>
+      <div className={styles["floating-course-content"]}>
+        <h2 className={styles["price"]}>E£679.99</h2>
+        <button className={styles["btn"]}>Buy now</button>
       </div>
     </header>
   );
