0c77f385b1f4db40391956eaa54e4dfb6c1717be
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-09-09 01:14:31 +0200

    feat: create useFetch custome hook

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchResponse = (response) => {
+      if (!response.ok) {
+        throw Error("Couldn't fetch data please try again");
+      }
+      return response.json();
+    };
+    const setResponse = (data) => {
+      setData(data);
+      setIsPending(false);
+      setError(null);
+    };
+
+    const handleFetchError = (error) => {
+      if (error.name === "AbortError") return;
+      setError(error.message);
+      setIsPending(false);
+    };
+
+    const abortController = new AbortController();
+
+    fetch(url, { signal: abortController.signal })
+      .then(fetchResponse)
+      .then(setResponse)
+      .catch(handleFetchError);
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
